915ce517688e49710b7299e155c4412169311542
AleksandrSoiko <farshchef777@gmail.com>
2023-02-19 18:56:49 +0000

    Update TransactionItem.jsx

--- src/components/Transaction/TransactionItem.jsx
+++ src/components/Transaction/TransactionItem.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import { BodyColumn } from './Transaction.styled';
 
 export const TransactionItem = ({ item }) => {
@@ -8,4 +9,12 @@ export const TransactionItem = ({ item }) => {
       <BodyColumn>{item.currency}</BodyColumn>
     </>
   );
+};
+
+TransactionItem.propTypes = {
+  item: PropTypes.shape({
+    type: PropTypes.string.isRequired,
+    amount: PropTypes.string.isRequired,
+    currency: PropTypes.string.isRequired,
+  }).isRequired,
 };
