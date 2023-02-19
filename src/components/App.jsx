cb63b5bf5bf1fe02d8a00fb4273fcc2824ef1667
AleksandrSoiko <farshchef777@gmail.com>
2023-02-19 18:08:41 +0000

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import { Profile } from './Profile/Profile';
 import { Layout } from './Layout/Layout';
-// import { StatBlock } from './Statistic/StatisticList';
+import { StatBlock } from './Statistic/StatisticList';
 import { FriendList } from './FriendList/FriendList';
 import { TransactionList } from './Transaction/TransactionList';
 import user from '../data/user.json';
@@ -12,7 +12,7 @@ export const App = () => {
   return (
     <Layout>
       <Profile user={user} />
-      {/* <StatBlock items={items} title={'Statistic'} /> */}
+      <StatBlock items={items} title={'Statistic'} />
       <FriendList items={friends} />
       <TransactionList items={transaction} />
     </Layout>
