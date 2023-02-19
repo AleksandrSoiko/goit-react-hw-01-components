a2f7efa024e08c5394e984487c5c8d1e53750b88
AleksandrSoiko <farshchef777@gmail.com>
2023-02-19 18:38:48 +0000

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,8 @@
 import { Profile } from './Profile/Profile';
 import { Layout } from './Layout/Layout';
-import { StatBlock } from './Statistic/StatisticList';
 import { FriendList } from './FriendList/FriendList';
 import { TransactionList } from './Transaction/TransactionList';
 import user from '../data/user.json';
-import items from '../data/data.json';
 import friends from '../data/friends.json';
 import transaction from '../data/transactions.json';
 
@@ -12,7 +10,6 @@ export const App = () => {
   return (
     <Layout>
       <Profile user={user} />
-      <StatBlock items={items} title={'Statistic'} />
       <FriendList items={friends} />
       <TransactionList items={transaction} />
     </Layout>
